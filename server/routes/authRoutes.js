a663b46f85cbcfee56794af4038dd0b67a465f5e
Dany Azriel <danyazriel@Danys-MacBook-Pro.local>
2023-01-23 01:19:15 +0200

    finish auth flow

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,7 +8,22 @@ module.exports = (app) => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
+
+  app.get('/', (req, res) => {
+    res.redirect('/auth/google');
+  });
+
+  app.get(
+    '/surveys', (req, res) => {
+      res.send({ hello: 'sucker' });
+  });
 
   app.get('/api/logout', (req, res) => {
     req.logout();
